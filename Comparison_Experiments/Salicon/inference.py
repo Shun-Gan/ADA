55d14deb3a96c41799d0bf3a40cc3d1bc9c901e4
Shun-Gan <75718549+Shun-Gan@users.noreply.github.com>
2022-01-01 10:22:38 +0800

    Add files via upload

---
+++ Comparison_Experiments/Salicon/inference.py
@@ -0,0 +1,112 @@
+import os, glob, torch, cv2
+import pandas as pd
+import numpy as np
+from model import Salicon
+
+
+class Example_prediction():
+    def __init__(self, ):
+        self.root = './runs/'
+        self.img_path = '../inference/'
+        self.img_size = [224, 384]
+        self.num_frames = 32
+        self.net = Salicon()
+        self.write_video=True
+        self.net = self.net.cuda()
+        self.folders = sorted(os.listdir(self.root))
+
+    def read_img_seq(self, imgs):
+        fine_img_seq=[]
+        coarse_img_seq=[]
+        for img_path in imgs:
+            img = cv2.imread(img_path)
+            fine_img = img.astype('float32')/255.
+            fine_img_seq.append(fine_img.transpose(2, 0, 1))
+
+            coarse_img = cv2.resize(img, (self.img_size[1], self.img_size[0]))
+            coarse_img = coarse_img.astype('float32')/255.
+            coarse_img_seq.append(coarse_img.transpose(2, 0, 1))
+        return coarse_img_seq, fine_img_seq
+
+    def write_img(self, path, img):
+        img = (img/img.max()*255)
+        img = np.array(img.detach().cpu().numpy(), dtype=np.uint8)
+        cv2.imwrite(path, img)
+
+    def inference(self,):
+        for folder in self.folders:
+            dataset = folder.split('_')[0]
+            weight = self.root+folder+'/best_weight_%s.pt'%dataset
+            self.net.load_state_dict(torch.load(weight))
+            print('load weight file', weight)
+            root_path = self.root+folder+'/inference/'
+            if not os.path.exists(root_path):
+                os.makedirs(root_path)
+            self.predict_example(root_path)
+
+    def predict_example(self, root_path):
+        img_folders = os.listdir(self.img_path)
+        for img_folder in img_folders:
+            imgs = glob.glob(self.img_path+img_folder+'/*/images/*.png')
+            output_path = os.path.join(root_path, img_folder, 
+                                        imgs[0].split(os.sep)[1])
+            if not os.path.exists(output_path):
+                os.makedirs(output_path)
+            coarse_img_seq, fine_img_seq = self.read_img_seq(imgs) # [c, h, w]*num
+            
+            self.net.eval()
+            for idx, coarse_img in enumerate(coarse_img_seq):
+                coarse_img = torch.from_numpy(coarse_img)
+                coarse_img = torch.unsqueeze(coarse_img, dim=0) # [1, c, num, h, w]
+                fine_img = torch.from_numpy(fine_img_seq[idx])
+                fine_img = torch.unsqueeze(fine_img, dim=0)
+
+                logits = self.net(fine_img.cuda(), coarse_img.cuda())
+                img_path = output_path + '/%s.png'%(str(idx+1).zfill(4))
+                self.write_img(img_path, torch.squeeze(logits))
+
+            if self.write_video:
+                self.generate_video(output_path, imgs)
+
+    def generate_video(self, video_path, images):
+
+        freq_dict = {'DADA2000':30, 'BDDA':30, 'EyeTrack':24, 'DReye':25}
+        fourcc= cv2.VideoWriter_fourcc(*'XVID')
+        source = video_path.split(os.sep)[-2].split('/')[-1]
+        Freq = freq_dict[source]
+
+        video_name = video_path+'_heatmap.avi'
+        if os.path.exists(video_name):
+            print('pass: ', video_name)
+        else:
+            if len(images)>0:
+                img_size = cv2.imread(images[0]).shape[:2]
+                out = cv2.VideoWriter (video_name, \
+                    fourcc, Freq, (img_size[1],img_size[0]))
+                for img in images[self.num_frames-1:]:
+                    gaze_heatmap = self.heat_map(img, img_size, video_path)
+                    out.write(gaze_heatmap)
+                
+                out.release()
+                print('write the video:', video_name)
+            else:
+                pass
+
+    def heat_map(self, img, img_size, video_path):
+
+        gaze_map_path = os.path.join(video_path, img.split(os.sep)[-1])
+
+        image = cv2.imread(img)
+        gaze_map = cv2.imread(gaze_map_path, 0)
+        gaze_map = cv2.resize(gaze_map, (img_size[1], img_size[0]))
+        heatmap = cv2.applyColorMap(gaze_map, cv2.COLORMAP_JET)
+        gaze_heatmap =cv2.addWeighted(image,0.5,heatmap,0.5,0)
+        
+        return gaze_heatmap
+
+
+
+if __name__=="__main__":
+    Predictor = Example_prediction()
+    Predictor.inference()
+
---
+++ Comparison_Experiments/Salicon/model.py
@@ -0,0 +1,28 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+import torchvision.models as models
+from functools import partial
+
+class Salicon(nn.Module):
+    def __init__(self):
+        super(Salicon,self).__init__()
+        self.Vgg16_fine_features=models.vgg16(pretrained=True).features[0:30]
+        self.Vgg16_coarse_features=models.vgg16(pretrained=True).features[0:30]
+
+        self.Unsample=partial(F.interpolate,mode='nearest')
+        self.Sal_map=nn.Conv2d(1024,1,(1,1))
+        self.Sigmoid=nn.Sigmoid()
+
+    def forward(self,raw_img,resized_img):
+        raw_features=self.Vgg16_fine_features(raw_img)
+        resized_features=self.Vgg16_coarse_features(resized_img)
+        H,W=raw_features.shape[-2],raw_features.shape[-1]
+
+        resized_features=self.Unsample(resized_features,size=[H,W])
+
+        out=torch.cat([raw_features,resized_features],dim=1)
+        out=self.Sal_map(out)
+        out=self.Sigmoid(out)
+
+        return out
